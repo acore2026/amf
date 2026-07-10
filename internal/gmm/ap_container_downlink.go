6bc20404dca0cdd831169b6bd48544b96f07320d
acore2026 <acore2026@example.com>
2026-07-10 14:34:15 +0800

    feat: build fragmented DL AP Containers

---
+++ internal/gmm/ap_container_downlink.go
@@ -0,0 +1,91 @@
+package gmm
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/acore2026/nas/nasMessage"
+)
+
+func buildDLAPContainerIEs(
+	messageIdentity uint8,
+	complete *nasMessage.APContainer,
+) ([]*nasMessage.CooperationIE, error) {
+	if complete == nil {
+		return nil, fmt.Errorf("complete AP Container is nil")
+	}
+	if complete.DontFragment() {
+		fragment := cloneCompleteAPContainer(complete)
+		fragment.ContainerFlags = nasMessage.APContainerFlagDF
+		fragment.FragmentOffset = 0
+		return buildSingleDLAPContainerIE(messageIdentity, fragment)
+	}
+	if len(complete.Payload) == 0 {
+		fragment := cloneCompleteAPContainer(complete)
+		fragment.ContainerFlags = 0
+		fragment.FragmentOffset = 0
+		return buildSingleDLAPContainerIE(messageIdentity, fragment)
+	}
+
+	result := make([]*nasMessage.CooperationIE, 0,
+		(len(complete.Payload)+nasMessage.APContainerMaxDLFragmentSize-1)/
+			nasMessage.APContainerMaxDLFragmentSize)
+	for offset := 0; offset < len(complete.Payload); offset += nasMessage.APContainerMaxDLFragmentSize {
+		end := offset + nasMessage.APContainerMaxDLFragmentSize
+		if end > len(complete.Payload) {
+			end = len(complete.Payload)
+		}
+		flags := uint8(0)
+		if end < len(complete.Payload) {
+			flags |= nasMessage.APContainerFlagMF
+		}
+		fragment := &nasMessage.APContainer{
+			ContainerType:      complete.ContainerType,
+			ContainerTypePTI:   complete.ContainerTypePTI,
+			ContainerPayloadID: complete.ContainerPayloadID,
+			ContainerFlags:     flags,
+			FragmentOffset:     uint16(offset),
+			Payload:            append([]byte(nil), complete.Payload[offset:end]...),
+		}
+		ie, err := encodeDLAPContainerIE(messageIdentity, fragment)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, ie)
+	}
+	return result, nil
+}
+
+func buildSingleDLAPContainerIE(
+	messageIdentity uint8,
+	container *nasMessage.APContainer,
+) ([]*nasMessage.CooperationIE, error) {
+	ie, err := encodeDLAPContainerIE(messageIdentity, container)
+	if err != nil {
+		return nil, err
+	}
+	return []*nasMessage.CooperationIE{ie}, nil
+}
+
+func encodeDLAPContainerIE(
+	messageIdentity uint8,
+	container *nasMessage.APContainer,
+) (*nasMessage.CooperationIE, error) {
+	contents, err := container.Encode()
+	if err != nil {
+		return nil, err
+	}
+	if messageIdentity == 0x01 {
+		return nasMessage.NewCooperationIE(nasMessage.CooperationIEType71, contents)
+	}
+	if len(contents) > math.MaxUint16 {
+		return nil, fmt.Errorf("legacy AP Container length %d exceeds 65535", len(contents))
+	}
+	return nasMessage.NewCooperationIELegacy(nasMessage.CooperationIEType71, contents), nil
+}
+
+func cloneCompleteAPContainer(in *nasMessage.APContainer) *nasMessage.APContainer {
+	out := *in
+	out.Payload = append([]byte(nil), in.Payload...)
+	return &out
+}
---
+++ internal/gmm/ap_container_downlink_test.go
@@ -0,0 +1,120 @@
+package gmm
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/acore2026/nas/nasMessage"
+)
+
+func TestBuildDLAPContainerIEsFragmentsAt245Bytes(t *testing.T) {
+	payload := make([]byte, 500)
+	for i := range payload {
+		payload[i] = byte(i)
+	}
+	complete := &nasMessage.APContainer{
+		ContainerType:      0x0100,
+		ContainerTypePTI:   0x05,
+		ContainerPayloadID: 0x1234,
+		Payload:            payload,
+	}
+
+	ies, err := buildDLAPContainerIEs(0x01, complete)
+	if err != nil {
+		t.Fatalf("buildDLAPContainerIEs() error = %v", err)
+	}
+	if len(ies) != 3 {
+		t.Fatalf("IE count = %d, want 3", len(ies))
+	}
+	wantOffsets := []uint16{0, 245, 490}
+	wantLengths := []int{245, 245, 10}
+	reassembled := make([]byte, 0, len(payload))
+	for i, ie := range ies {
+		fragment, err := nasMessage.DecodeAPContainer(ie.GetContents())
+		if err != nil {
+			t.Fatalf("fragment %d decode error = %v", i, err)
+		}
+		if fragment.FragmentOffset != wantOffsets[i] || len(fragment.Payload) != wantLengths[i] {
+			t.Fatalf("fragment %d offset=%d length=%d", i, fragment.FragmentOffset, len(fragment.Payload))
+		}
+		if fragment.MoreFragments() != (i < 2) || fragment.DontFragment() {
+			t.Fatalf("fragment %d DF=%v MF=%v", i, fragment.DontFragment(), fragment.MoreFragments())
+		}
+		reassembled = append(reassembled, fragment.Payload...)
+	}
+	if !bytes.Equal(reassembled, payload) {
+		t.Fatal("DL fragments do not reconstruct the complete payload")
+	}
+}
+
+func TestBuildDLAPContainerIEsKeepsDFInOneMessage(t *testing.T) {
+	complete := &nasMessage.APContainer{
+		ContainerType:      0x0100,
+		ContainerTypePTI:   0x05,
+		ContainerPayloadID: 0x1234,
+		ContainerFlags:     nasMessage.APContainerFlagDF,
+		Payload:            bytes.Repeat([]byte{0xaa}, 245),
+	}
+	ies, err := buildDLAPContainerIEs(0x01, complete)
+	if err != nil {
+		t.Fatalf("buildDLAPContainerIEs() error = %v", err)
+	}
+	if len(ies) != 1 {
+		t.Fatalf("IE count = %d, want 1", len(ies))
+	}
+	fragment, err := nasMessage.DecodeAPContainer(ies[0].GetContents())
+	if err != nil {
+		t.Fatalf("DecodeAPContainer() error = %v", err)
+	}
+	if !fragment.DontFragment() || fragment.MoreFragments() || fragment.FragmentOffset != 0 {
+		t.Fatalf("DF fragment = %#v", fragment)
+	}
+}
+
+func TestBuildDLAPContainerIEsRejectsOversizedOneByteDFPayload(t *testing.T) {
+	complete := &nasMessage.APContainer{
+		ContainerFlags: nasMessage.APContainerFlagDF,
+		Payload:        bytes.Repeat([]byte{0xaa}, 246),
+	}
+	if _, err := buildDLAPContainerIEs(0x01, complete); err == nil {
+		t.Fatal("buildDLAPContainerIEs() error = nil")
+	}
+}
+
+func TestBuildDLAPContainerIEsAllowsLargeLegacyDFPayload(t *testing.T) {
+	complete := &nasMessage.APContainer{
+		ContainerType:      0x0100,
+		ContainerTypePTI:   0x05,
+		ContainerPayloadID: 0x1234,
+		ContainerFlags:     nasMessage.APContainerFlagDF,
+		Payload:            bytes.Repeat([]byte{0xaa}, 300),
+	}
+	ies, err := buildDLAPContainerIEs(0x02, complete)
+	if err != nil {
+		t.Fatalf("buildDLAPContainerIEs() error = %v", err)
+	}
+	if len(ies) != 1 {
+		t.Fatalf("IE count = %d, want 1", len(ies))
+	}
+	if ies[0].LegacyLen != uint16(nasMessage.APContainerHeaderLength+len(complete.Payload)) {
+		t.Fatalf("LegacyLen = %d", ies[0].LegacyLen)
+	}
+}
+
+func TestBuildDLAPContainerIEsAllowsEmptyPayload(t *testing.T) {
+	complete := &nasMessage.APContainer{ContainerPayloadID: 1}
+	ies, err := buildDLAPContainerIEs(0x01, complete)
+	if err != nil {
+		t.Fatalf("buildDLAPContainerIEs() error = %v", err)
+	}
+	if len(ies) != 1 {
+		t.Fatalf("IE count = %d, want 1", len(ies))
+	}
+	fragment, err := nasMessage.DecodeAPContainer(ies[0].GetContents())
+	if err != nil {
+		t.Fatalf("DecodeAPContainer() error = %v", err)
+	}
+	if len(fragment.Payload) != 0 || fragment.FragmentOffset != 0 || fragment.MoreFragments() {
+		t.Fatalf("empty fragment = %#v", fragment)
+	}
+}
